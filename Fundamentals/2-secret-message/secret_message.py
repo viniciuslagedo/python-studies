91226646dbee1162743a8dc8ff5ce85ed57a0788
Vinicius Lagêdo <vinicius.lagedo@wooza.com.br>
2018-04-10 19:54:52 -0300

    Rename folder with lesson index

--- Fundamentals/2-secret-message/secret_message.py
+++ Fundamentals/2-secret-message/secret_message.py
@@ -2,8 +2,8 @@ import os
 #Remove numbers chars from a list of files names
 def rename_files():
     cwd_path = os.getcwd()
-    file_list = os.listdir(r"C:\\Users\\vinicius.lagedo\\Desktop\\testes\\Fundamentals\\secret-message\\secret-images")
-    os.chdir(r"C:\\Users\\vinicius.lagedo\\Desktop\\testes\\Fundamentals\\secret-message\\secret-images")
+    file_list = os.listdir(r"C:\\Users\\vinicius.lagedo\\Desktop\\testes\\Fundamentals\\2-secret-message\\secret-images")
+    os.chdir(r"C:\\Users\\vinicius.lagedo\\Desktop\\testes\\Fundamentals\\2-secret-message\\secret-images")
     for file_name in file_list:
         new_name = file_name.translate(None, "0123456789")
         print("From " + file_name + " to " + new_name)
