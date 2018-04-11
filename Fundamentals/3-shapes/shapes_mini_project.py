d958d344ad303d99967266136828c2f23e360a8e
Vinicius Lagêdo <vinicius.lagedo@wooza.com.br>
2018-04-11 13:34:35 -0300

    Add mini project to shapes lesson

--- Fundamentals/3-shapes/shapes_mini_project.py
+++ Fundamentals/3-shapes/shapes_mini_project.py
@@ -1,1 +1,26 @@
-#Draw initials of name
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("blue")
+
+    pointer = turtle.Turtle()
+    pointer.shape("turtle")
+    pointer.color("white")
+    pointer.speed(2)
+
+    pointer.right(65)
+    pointer.forward(100)
+    pointer.right(-130)
+    pointer.forward(100)
+
+    pointer.penup()
+    pointer.setx(100)
+    pointer.setheading(270)
+
+    pointer.pendown()
+    pointer.forward(100)
+    pointer.left(90)
+    pointer.forward(100)
+        
+    window.exitonclick()
+
+draw_art()
